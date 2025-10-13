e038c2869f1a618bbe7de8b956f72731c829fbd2
imrehov <114602851+imrehov@users.noreply.github.com>
2025-10-13 20:04:16 +0200

    c12

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import postgres from 'postgres';
 import { redirect } from 'next/navigation';
+import Form from '../ui/invoices/create-form';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -14,6 +15,8 @@ const FormSchema = z.object({
 	status: z.enum(['pending', 'paid']),
 	date: z.string(),
 });
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
@@ -35,4 +38,27 @@ export async function createInvoice(formData: FormData) {
 	revalidatePath('/dashboard/invoices');
 	redirect('/dashboard/invoices');
 
+
+}
+export async function updateInvoice(id: string, formData: FormData) {
+	const { customerId, amount, status } = UpdateInvoice.parse({
+		customerId: formData.get('customerId'),
+		amount: formData.get('amount'),
+		status: formData.get('status'),
+	});
+
+	const amountInCents = amount * 100;
+
+	await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+	revalidatePath('/dashboard/invoices');
+	redirect('/dashboard/invoices');
+}
+export async function deleteInvoice(id: string) {
+	await sql`DELETE FROM invoices WHERE id = ${id}`;
+	revalidatePath('/dashboard/invoices');
 }
